4673687afa70eab20cb6ae9911ca14117ddaa95b
PavithraKowshik <58665329+PavithraKowshik@users.noreply.github.com>
2021-04-07 13:44:28 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+class Car extends React.Component {
+  render() {
+    return <h2>This car is {this.props.brand}!</h2>;
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+    
+      <Car brand="Ford" />
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Garage />, document.getElementById('root'));
+
+
